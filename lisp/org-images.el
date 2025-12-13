90cb3befc44d47360a17c79ac26b868055f29319
gorion <gorion.green@gmail.com>
2025-12-13 22:11:37 +0300

    Add org-images module

---
+++ lisp/org-images.el
@@ -0,0 +1,10 @@
+;;; org-images.el --- Image handling for Org mode
+
+;; Automatically display images inline
+(setq org-startup-with-inline-images t)
+
+;; Allow custom scaling
+(setq org-image-actual-width nil)
+
+(provide 'org-images)
+;;; org-images.el ends here
--- lisp/ui.el
+++ lisp/ui.el
@@ -1,25 +1,30 @@
 ;;; ui.el --- Basic UI settings
 
 ;; Turn of unnecesary UI elements
-;; These settings make Emacs minimal and distraction-free
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
+;; Dark theme
+(load-theme 'modus-vivendi t)
+
 ;; Disable startup screen
-;; (setq inhabit-startup-message t)
+(setq inhibit-startup-message t)
 
 ;; Show line numbers
-(global-display-line-numbers-mode 1)
+(global-display-line-numbers-mode -1)
 
 ;; Highlight current line
 (global-hl-line-mode 1)
 
-;; Set a gentle default font size
-(set-face-attribute 'default nil :height 120)
+ ;; Scale relative to column
+(setq org-image-actual-width nil)
 
-;; Soft wrap long lines
+;; Wraps text at the edge of the window
 (global-visual-line-mode 1)
+
+;; Font size 14pt
+(set-face-attribute 'default nil :height 140)
 
 (provide 'ui)
 ;;; ui.el ends here
